e1c0e9c15f0d161c74b82939162b10d17bdac550
Mingye Chen <61096358+mingyeeee@users.noreply.github.com>
2021-05-31 22:19:11 -0400

    added positivity counter

--- Teensy-Shoulder-Check/arduino_detection_responder.cpp
+++ Teensy-Shoulder-Check/arduino_detection_responder.cpp
@@ -1,6 +1,6 @@
-/* 
+/*
  * Modified by: Mingye Chen
- * Date: 2021-03-07
+ * Date: 2021-05-31
  * Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -27,33 +27,29 @@ void RespondToDetection(tflite::ErrorReporter* error_reporter,
   if (!is_initialized) {
     // Pins for the built-in RGB LEDs on the Arduino Nano 33 BLE Sense
     pinMode(2, OUTPUT);
-    pinMode(1, OUTPUT);
-    pinMode(0, OUTPUT);
+    postivityCount = 0;
     is_initialized = true;
   }
 
   // Note: The RGB LEDs on the Arduino Nano 33 BLE
   // Sense are on when the pin is LOW, off when HIGH.
 
-  // Switch the person/not person LEDs off
-  digitalWrite(1, LOW);
+  // Switch the person LED off
   digitalWrite(2, LOW);
-
-  // Flash the blue LED after every inference.
-  digitalWrite(0, LOW);
-  delay(100);
-  digitalWrite(0, HIGH);
 
   // Switch on the green LED when a person is detected,
   // the red when no person is detected
   if (person_score > no_person_score) {
-    digitalWrite(1, LOW);
-    digitalWrite(2, HIGH);
     TF_LITE_REPORT_ERROR(error_reporter, "Person detected!");
+    postivityCount ++;
   } else {
-    digitalWrite(1, HIGH);
     digitalWrite(2, LOW);
     TF_LITE_REPORT_ERROR(error_reporter, "NO ONE detected!");
+    postivityCount = 0;
+  }
+  // Check how many consecutive times a person was detected
+  if (postivityCount > 5){
+    digitalWrite(2, HIGH);
   }
   TF_LITE_REPORT_ERROR(error_reporter, "Person score: %d No person score: %d",
                        person_score, no_person_score);
